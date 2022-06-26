82b718316ecdc0f98c5bdf8748a1b92f95c8db9c
ariefsamuel <samuel.arief21@gmail.com>
2022-06-26 14:33:10 +0700

    challenge 09

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,16 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({data}) => {
     return (
-        <></>
+        <div className='person'>
+            <img src={data.photo} alt='photo-person' className='avatar' />
+            <div className='profile'>
+                <h1 className='name'>{data.name}</h1>
+                <h1 className='phone'>{data.phone}</h1>
+                <h1 className='email'>{data.email}</h1>
+            </div>
+        </div>
     )
 }
 
